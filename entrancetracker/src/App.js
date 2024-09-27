e6fe32f4179b29d5d308c7428c23aa1593201cad
midnight-grind <mcpersonforty@gmail.com>
2024-09-26 22:44:26 -0400

    added materials

--- entrancetracker/src/App.js
+++ entrancetracker/src/App.js
@@ -1,28 +1,56 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import GridViewTwoToneIcon from '@mui/icons-material/GridViewTwoTone';
+import Tooltip from '@mui/material/Tooltip';
+
 
 function App() {
   const items = [1, 2, 3];
   return (
     <div className="App">
       <header className="App-header">
+
+      <AddCircleTwoToneIcon style={{ fontSize: 40, color: 'green' }} />
+      <TextField
+          id="outlined-required"
+          label="From"
+          defaultValue="Deku Tree"
+        /><br></br>
+        <TextField
+          id="outlined-required"
+          label="To"
+          defaultValue="Kokiri Forest"
+        />
         <div>
           {items.map((item) => (
-            <ShowLocations key={item} />
+            <Locations key={item} />
           ))}
-          <ShowSpaces/>
         </div>
       </header>
     </div>
   );
 }
 
-function ShowLocations() {
-  return <span className="box">test</span>;
+function Locations() {
+  return (
+    <React.Fragment>
+      <span className="box">test</span>
+      <span className="space"></span>
+    </React.Fragment>
+  )
+
+  // return (
+  //   <div className="wrapper">
+  //     <span className="box">test</span>
+  //     <span className="space"></span>
+  //   </div>
+  // );
 }
 
-function ShowSpaces() {
-  return <span className="space"></span>
-}
+// function 
 
 export default App;
